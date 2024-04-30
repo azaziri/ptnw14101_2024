58dcd168a05575797215088c0c8b9ef2a139c288
azaziri <aris.azaziri22@gmail.com>
2024-04-30 09:15:21 +0700

    add html di pertemuan 10

---
+++ belajar/pertemuan10/latihan1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Buku</title>
+</head>
+
+<body>
+  <h3>Daftar Buku</h3>
+
+  <table border="1" cellpadding="10" cellspasing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>SKU</th>
+      <th>Judul</th>
+      <th>Tahun</th>
+      <th>Penulis</th>
+      <th>Kategori</th>
+      <th>Deskripsi</th>
+      <th>Aksi</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td><img src="" alt="no-image"></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </table>
+</body>
+
+</html>
